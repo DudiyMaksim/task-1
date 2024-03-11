0c4fa09f84601c5aca490ed1110627d6e7e473ee
[DudiyMaksim] <[maksdudiya25@gmail.com]>
2024-03-11 19:26:55 +0200

    Add project files.

---
+++ FileName.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main() {
+	///*task 1*/
+	//float R1, R2, R3;
+	//cout << "R1  : \n";
+	//cin >> R1;
+	//cout << "R2  : \n";
+	//cin >> R2;
+	//cout << "R3  : \n";
+	//cin >> R3;
+	//cout << 1/(1/R1+1/R2+1/R3);
+	///task 2;
+	//short L;
+	//cout << "L  : \n";
+	//cin >> L;
+	//cout << "S = " <<3.14 * pow(L / (2 * 3.14), 2);
+	////task 3
+	//short v, t, a;
+	//cout << "input v t and a :\n";
+	//cin >> v >> t >> a;
+	//cout << "s = " << v * t + (a * t * 2) / 2;
+	////task 4
+	//short a;
+	////чи float
+	//cout << "Input a : \n\t";
+	//cin >> a;
+	//cout << "in hours : " << a / 60 / 60 << "\nin minutes : " << a / 60 << "\nin seconds : " << a;
+	////task 5
+	//float a;
+	//cout << "Input a : \n\t";
+	//cin >> a;
+	//cout << int(a) << " grn " << (a - int(a))*100<< " cop";
+	////task 6 
+	//short d;
+	//double t;
+	//cout << "Calculating running speed" << endl;
+	//cout << "Input distance (meters):  ";
+	//cin >> d;
+	//cout << "Input time(min.sec) :  ";
+	//cin >> t;
+	//cout << "Distance : " << d << " m" << endl;
+	//cout << "Time : " << int(t) << " min " << (t - int(t)) * 100 << " sec = " << int(t) * 60 + (t - int(t)) * 100 <<" sec"<< endl;
+	//cout << "Your speed : " << (d/(int(t) * 60 + (t - int(t)) * 100))*3600/1000 << " km/h";
+	////task 7
+	//short d;
+	//cout << "Input d :";
+	//cin >> d;
+	//cout << d / 7 << " Weeks and " << d % 7 << " days";
+}
